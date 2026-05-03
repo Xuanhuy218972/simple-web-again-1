7f8a5a5413e4a7268eb0b7634df08064abe1c2c4
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2026-05-03 10:01:27 +0700

    Refactor Router to support DSL-based route definition

--- config.ru
+++ config.ru
@@ -1,3 +1,11 @@
+$LOAD_PATH.unshift << '.'
 require_relative "lib/frack/application"
+require_relative "lib/frack/router"
+require_relative "lib/frack/base_controller"
+require_relative "app/controllers/home_controller"
 
-run Frack::Application.new
+
+run Frack::Router.new(Frack::Application) {
+    get "/"      => "home#show"
+    get "/users" => "users#new"
+}
--- lib/frack/router.rb
+++ lib/frack/router.rb
@@ -1,14 +1,31 @@
 module Frack
     class Router
-        def initialize
-            @routes = {
-                "/" => "HomeController#show",
-                "/users/new" => "UsersController#new",
-            }
+        attr_reader :routes, :app
+        def initialize(app, &block)
+            @app    = app
+            @routes = {}
+            instance_eval(&block) if block_given?
         end
 
-        def route(path)
-            @routes[path]  || "404 Not Found"
+        def call(env)
+            path = env['PATH_INFO']
+            http_method = env['REQUEST_METHOD']
+            if mapping = routes[path + http_method] 
+                env.merge!(controller_action(mapping))
+            app.call(env)
+            else
+                [404, {'content-type' => 'text/plain'}, ['Not Found']]
+            end
+        end
+
+        def controller_action(mapping)
+            controller, action = mapping.split('#')
+            {"controller" => controller, "action" => action}
+        end
+
+        def get(route)
+            path, mapping = route.first
+            routes[path + "GET"] = mapping
         end
     end
 end
