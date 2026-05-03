ba4a1d1a75af86147862784a01f93274fe2664b8
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2026-05-03 10:01:47 +0700

    Update Application to handle dynamic controller dispatching

--- lib/frack/application.rb
+++ lib/frack/application.rb
@@ -1,12 +1,21 @@
-require_relative "router"
-
 module Frack
     class Application
-        def call(env)
-            path = env["PATH_INFO"]
-            handler = Router.new.route(path)
-
-            [200, { "content-type" => "text/plain" }, ["Routing to: #{handler}"]]
+        class << self
+            attr_accessor :env
+            def call(env)
+                self.env = env
+                body = dispatch
+                [200, { "content-type" => "text/html" }, [body]]
+            
+            end
+            
+            def dispatch
+                controller.new(env).public_send(env["action"])
+            end
+        
+            def controller
+                Object.const_get(env["controller"].capitalize + "Controller")
+            end
         end
     end
 end
