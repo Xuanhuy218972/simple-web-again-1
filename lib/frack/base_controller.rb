37bf7d4ec0265373c10e68dd68f0f7bfa7e72a16
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2026-05-04 15:03:40 +0700

    refactor: implement nested rendering using tilt and er

--- lib/frack/base_controller.rb
+++ lib/frack/base_controller.rb
@@ -1,23 +1,33 @@
-require 'erb'
+require 'tilt'
+require 'erubi'
 
 module Frack
-    class BaseController
-        attr_reader :env
+  class BaseController
+    attr_reader :env
 
-        def initialize(env)
-            @env = env
-        end
-
-        def render(view_name)
-            path = "app/views/#{controller_name}/#{view_name}.html.erb"
-            template = File.read(path)
-            ERB.new(template).result(binding)
-        end
-
-        private
-
-        def controller_name
-            self.class.name.gsub("Controller", "").downcase
-        end
+    def initialize(env)
+      @env = env
     end
+
+    def render(view)
+      render_template('layouts/application') do
+        render_template("#{controller_name}/#{view}")
+      end
+    end
+
+    def render_template(path, &block)
+      template = Tilt::ErubiTemplate.new(file(path))
+      template.render(self, &block)
+    end
+
+    private
+
+    def file(path)
+      File.expand_path("app/views/#{path}.html.erb")
+    end
+
+    def controller_name
+      self.class.name.gsub("Controller", "").downcase
+    end
+  end
 end
